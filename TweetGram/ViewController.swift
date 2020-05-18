3d9f659aaee0636602530290934fae1ce038e047
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-18 20:05:56 +0300

    Initial Commit

---
+++ TweetGram/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TweetGram
+//
+//  Created by Aleksandr Nikiforov on 18.05.2020.
+//  Copyright © 2020 Aleksandr Nikiforov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
